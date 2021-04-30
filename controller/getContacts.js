b3973221606e80204476ddff1907c0dfd9c06568
erislash <eri.gim2008@gmail.com>
2021-04-30 20:28:45 -0300

    set up

--- controller/getContacts.js
+++ controller/getContacts.js
@@ -1,7 +1,6 @@
 const getContacts = require('../model/getContacts');
 module.exports = (req, res) => {
     getContacts(contacts => {
-        console.log(contacts);
         res.render('contacts.ejs', {contacts});
     });
 }
